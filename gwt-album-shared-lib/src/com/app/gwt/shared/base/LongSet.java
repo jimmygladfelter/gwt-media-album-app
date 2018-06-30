2886d0f33b99783a045a52211aa2e1893d0a4e74
James C. Gladfelter <James C. Gladfelter@jgladfelter-pc>
2018-06-29 21:10:04 -0400

    Added more dtos.

---
+++ gwt-album-shared-lib/src/com/app/gwt/shared/base/LongSet.java
@@ -0,0 +1,224 @@
+package com.app.gwt.shared.base;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Iterator;
+
+/**
+ * Represents a set of long integers that can be added to and searched.
+ * 
+ * @author James C. Gladfelter
+ *
+ */
+public class LongSet implements Serializable, Iterable<Long> {
+
+	private static final long serialVersionUID = -5753931690986729670L;
+
+	protected HashSet<Long> longs;
+
+	/**
+	 * Creates a new Long Integer set.
+	 */
+	public LongSet() {
+		super();
+	}
+
+	/**
+	 * Creates a new Long Integer set.
+	 * 
+	 * @param Longs
+	 */
+	public LongSet(HashSet<Long> longs) {
+		super();
+		this.longs = longs;
+	}
+
+	/**
+	 * Retrieves the long integers that correspond to this object.
+	 * 
+	 * @return long integers that correspond to this object.
+	 */
+	public HashSet<Long> getLongs() {
+		return this.longs;
+	}
+
+	/**
+	 * Retrieves the number of Longs in the set.
+	 * 
+	 * @return the number of Longs in the set.
+	 */
+	public int size() {
+		return this.longs != null ? this.longs.size() : 0;
+	}
+
+	/**
+	 * Clears all Longs from the set.
+	 */
+	public void clear() {
+		if (this.longs != null) {
+			this.longs.clear();
+			this.longs = null;
+		}
+	}
+
+	/**
+	 * Determines if there are any Longs in the set.
+	 * 
+	 * @return true / false whether or not there are any Longs in the set.
+	 */
+	public boolean isEmpty() {
+		return this.longs != null ? this.longs.isEmpty() : true;
+	}
+
+	@Override
+	public Iterator<Long> iterator() {
+		return this.longs != null ? this.longs.iterator() : new Iterator<Long>() {
+
+			@Override
+			public boolean hasNext() {
+				return false;
+			}
+
+			@Override
+			public Long next() {
+				return null;
+			}
+		};
+	}
+
+	/**
+	 * Sets long integers that correspond to this object.
+	 * 
+	 * @param longs
+	 *            - long integers that correspond to this object.
+	 */
+	public void setLongs(HashSet<Long> longs) {
+		this.longs = null;
+		if (longs != null) {
+			this.longs = new HashSet<Long>();
+			for (Long l : longs) {
+				if (l == null) {
+					continue;
+				}
+				this.longs.add(l);
+			}
+			if (this.longs.isEmpty()) {
+				this.longs = null;
+			}
+		}
+	}
+
+	/**
+	 * Adds a long integer to the object.
+	 * 
+	 * @param l
+	 *            - a long integer.
+	 * @return true / false whether the long integer was added or not.
+	 */
+	public boolean addLong(Long l) {
+		boolean added = false;
+		if (l != null) {
+			if (this.longs == null) {
+				this.longs = new HashSet<Long>();
+			}
+			added = this.longs.add(l);
+		}
+		return added;
+	}
+
+	/**
+	 * Adds an array of long integers to the object.
+	 * 
+	 * @param Longs
+	 *            - array of long integers to the object.
+	 */
+	public void addLongs(Long... longs) {
+		if (longs != null) {
+			for (Long l : longs) {
+				this.addLong(l);
+			}
+		}
+	}
+
+	/**
+	 * Determines if a long integer is associated with this object.
+	 * 
+	 * @param Long
+	 *            - the long integer to look for.
+	 * @return true / false whether or not the long integer is associated with the
+	 *         object.
+	 */
+	public boolean containsLong(Long l) {
+		boolean contains = false;
+		if (l != null) {
+			contains = this.longs.contains(l);
+		}
+		return contains;
+	}
+
+	/**
+	 * Determines if an array of long integers is associated with this object.
+	 * 
+	 * @param Longs
+	 *            - the long integers to look for.
+	 * @param ensureAllAreContained
+	 *            - determines if the contains function should return true if one is
+	 *            found or if all long integers in the specified array must be
+	 *            included.
+	 * @return true / false whether or not the array of long integers is associated
+	 *         with the object.
+	 */
+	public boolean containsLongs(boolean ensureAllAreContained, Long... longs) {
+		boolean contains = false;
+		if (this.longs != null && this.longs.size() > 0) {
+			int numValidLongs = 0;
+			int numContains = 0;
+			for (Long l : longs) {
+				if (l == null) {
+					continue;
+				}
+				boolean thisLongIncluded = this.containsLong(l);
+				if (thisLongIncluded) {
+					if (!ensureAllAreContained) {
+						contains = true;
+						break;
+					}
+					numContains++;
+				}
+				numValidLongs++;
+			}
+			contains = numValidLongs > 0 && numValidLongs == numContains;
+		}
+		return contains;
+	}
+
+	/**
+	 * 
+	 * Removes a long integer from the object.
+	 * 
+	 * @param l
+	 *            - a long integer.
+	 * @return true / false whether the long integer was removed or not.
+	 */
+	public boolean removeLong(Long l) {
+		boolean removed = false;
+		if (this.longs != null && l != null) {
+			removed = this.longs.remove(l);
+		}
+		return removed;
+	}
+
+	/**
+	 * Removes an array of long integers from the object.
+	 * 
+	 * @param Longs
+	 *            - array of long integers.
+	 */
+	public void removeLongs(Long... longs) {
+		if (this.longs != null && longs != null) {
+			for (Long l : longs) {
+				this.removeLong(l);
+			}
+		}
+	}
+}
---
+++ gwt-album-shared-lib/src/com/app/gwt/shared/template/AlbumPageTemplate.java
@@ -0,0 +1,98 @@
+package com.app.gwt.shared.template;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.app.gwt.shared.album.AlbumPage;
+import com.app.gwt.shared.base.LongSet;
+import com.app.gwt.shared.base.StringSet;
+import com.app.gwt.shared.comment.Comment;
+import com.app.gwt.shared.style.BackgroundStyle;
+
+/**
+ * Represents a page in an album.
+ * 
+ * @author James C. Gladfelter
+ */
+public class AlbumPageTemplate extends AlbumPage {
+
+	private static final long serialVersionUID = -465987982380216756L;
+
+	protected LongSet templatedEntryIds;
+
+	protected AlbumPageTemplate() {
+		// no-op - serializable
+	}
+
+	/**
+	 * Creates a new page template.
+	 * 
+	 * @param name
+	 * @param description
+	 * @param createdBy
+	 */
+	public AlbumPageTemplate(String name, String description, String createdBy) {
+		this(name, description, createdBy, new Date().getTime());
+	}
+
+	/**
+	 * Creates a new page template.
+	 * 
+	 * @param name
+	 * @param description
+	 * @param createdBy
+	 * @param dateCreated
+	 */
+	public AlbumPageTemplate(String name, String description, String createdBy, Long dateCreated) {
+		this(null, name, description, createdBy, dateCreated, createdBy, dateCreated, null, null, null, null, null,
+				null);
+	}
+
+	/**
+	 * Creates a new page template.
+	 * 
+	 * @param id
+	 * @param name
+	 * @param description
+	 * @param createdBy
+	 * @param dateCreated
+	 * @param lastModifiedBy
+	 * @param dateLastModified
+	 * @param likedByUsers
+	 * @param comments
+	 * @param tags
+	 * @param thumbnail
+	 * @param backgroundStyle
+	 * @param templatedEntryIds
+	 */
+	public AlbumPageTemplate(Long id, String name, String description, String createdBy, Long dateCreated,
+			String lastModifiedBy, Long dateLastModified, StringSet likedByUsers, ArrayList<Comment> comments,
+			StringSet tags, String thumbnail, BackgroundStyle backgroundStyle, LongSet templatedEntryIds) {
+		super(id, name, description, createdBy, dateCreated, lastModifiedBy, dateLastModified, likedByUsers, comments,
+				tags, thumbnail, backgroundStyle);
+		this.templatedEntryIds = templatedEntryIds;
+	}
+
+	/**
+	 * Retrieves a set of "templated" album page entries where the entry's media is
+	 * non-static (defined by the user when imported into an album).
+	 * 
+	 * @return a set of "templated" album page entries where the entry's media is
+	 *         non-static (defined by the user when imported into an album).
+	 */
+	public LongSet getTemplatedEntryIds() {
+		return this.templatedEntryIds;
+	}
+
+	/**
+	 * Sets the set of "templated" album page entries where the entry's media is
+	 * non-static (defined by the user when imported into an album).
+	 * 
+	 * @param templatedEntryIds
+	 *            - a set of "templated" album page entries where the entry's media
+	 *            is non-static (defined by the user when imported into an album).
+	 */
+	public void setTemplatedEntryIds(LongSet templatedEntryIds) {
+		this.templatedEntryIds = templatedEntryIds;
+	}
+}
